ff25427da76c3310bdabbf2dde410cacfb555a05
yradkhorrami <yaser.radkhorrami@desy.de>
2025-06-28 14:17:26 +0200

    Add python test functions

---
+++ file_test.py
@@ -0,0 +1,21 @@
+import pytest
+
+def test_calc_addition():
+	# function test the output of 2 + 4
+	output = 2 + 4
+	assert output == 6
+
+def test_calc_substraction():
+	# function test the output of 2 - 4
+	output = 2 - 4
+	assert output == -2
+
+def test_calc_multiply():
+	# function test the output of 2 * 4
+	output = 2 * 4
+	asser output == 8
+
+def test_coucou():
+	# function test if the output returns 'hello'
+	output = 'hello'
+	assert output == 'hello'
